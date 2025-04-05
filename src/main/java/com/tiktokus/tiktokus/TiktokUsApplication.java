5967dfe3d798eee34149b3b3ee96e6a325789179
TuananhTA <dungdhph33804@fpt.edu.vn>
2025-04-06 02:19:40 +0700

    first commit

---
+++ src/main/java/com/tiktokus/tiktokus/TiktokUsApplication.java
@@ -0,0 +1,13 @@
+package com.tiktokus.tiktokus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiktokUsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiktokUsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiktokus/tiktokus/TiktokUsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiktokus.tiktokus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiktokUsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
